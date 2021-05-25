3f51062bb4b65fb267dba71cc3302b6d12bd71c9
Aaron Dewes <aaron.dewes@web.de>
2021-05-25 10:36:08 +0200

    Unify code style in the document
    The coding style was different for pretty much every function, so Prettier fixed it now

--- lntrigger.ino
+++ lntrigger.ino
@@ -4,7 +4,7 @@
 
 #include <M5Stack.h>
 #include "FS.h"
-#include <WiFiManager.h> 
+#include <WiFiManager.h>
 #include <ArduinoJson.h>
 #include <WiFiClientSecure.h>
 #include "SPIFFS.h"
@@ -12,7 +12,7 @@
 /*               SOME DEFINES                 */
 
 #define LED_PIN 25
-  
+
 /*               SOME VARIABLES               */
 
 char lnbits_server[40] = "https://lnbits.com";
@@ -31,19 +31,19 @@ bool paid = false;
 bool shouldSaveConfig = false;
 bool down = false;
 
-const char* spiffcontent = "";
-String spiffing; 
-
+const char *spiffcontent = "";
+String spiffing;
 
 /*                   SETUP                     */
 
-void setup() {
+void setup()
+{
   M5.begin();
   logo_screen();
   delay(3000);
   lnbits_screen();
 
-   START PORTAL 
+  START PORTAL
 
   portal();
 }
@@ -52,58 +52,65 @@ void setup() {
 
 bool StartsWith(const char *a, const char *b)
 {
-   if(strncmp(a, b, strlen(b)) == 0) return 1;
-   return 0;
+  if (strncmp(a, b, strlen(b)) == 0)
+    return 1;
+  return 0;
 }
 
-int parseInt(const char* chars)
+int parseInt(const char *chars)
 {
-    int sum = 0;
-    int len = strlen(chars);
-    for (int x = 0; x < len; x++)
-    {
-        int n = chars[len - (x + 1)] - '0';
-        sum = sum + powInt(n, x);
-    }
-    return sum;
+  int sum = 0;
+  int len = strlen(chars);
+  for (int x = 0; x < len; x++)
+  {
+    int n = chars[len - (x + 1)] - '0';
+    sum = sum + powInt(n, x);
+  }
+  return sum;
 }
 
 int powInt(int x, int y)
 {
-    for (int i = 0; i < y; i++)
-    {
-        x *= 10;
-    }
-    return x;
+  for (int i = 0; i < y; i++)
+  {
+    x *= 10;
+  }
+  return x;
 }
 
 /*                 MAIN LOOP                   */
 
-void loop() {
-  pinMode (atoi(high_pin), OUTPUT);
+void loop()
+{
+  pinMode(atoi(high_pin), OUTPUT);
   digitalWrite(atoi(high_pin), LOW);
-  if(String(invoice_key) == ""){
+  if (String(invoice_key) == "")
+  {
     error_no_invoice_key_screen();
   }
   Serial.println(String(invoice_key));
   getinvoice();
 
-  if(down){
+  if (down)
+  {
     error_no_connection_screen();
     getinvoice();
     delay(5000);
   }
-  if(payReq != ""){
-   qrdisplay_screen();
-   delay(5000);
+  if (payReq != "")
+  {
+    qrdisplay_screen();
+    delay(5000);
   }
-  while(paid == false && payReq != ""){
+  while (paid == false && payReq != "")
+  {
     checkinvoice();
-    if(paid){
+    if (paid)
+    {
       complete_screen();
       digitalWrite(atoi(high_pin), HIGH);
       delay(atoi(time_pin));
-      digitalWrite(atoi(high_pin), LOW); 
+      digitalWrite(atoi(high_pin), LOW);
     }
     delay(3000);
   }
@@ -113,11 +120,10 @@ void loop() {
   delay(4000);
 }
 
-
 /*               DISPLAY                 */
 
 void logo_screen()
-{ 
+{
   M5.Lcd.fillScreen(WHITE);
   M5.Lcd.setCursor(0, 80);
   M5.Lcd.setTextSize(4);
@@ -125,9 +131,8 @@ void logo_screen()
   M5.Lcd.println("LNbitsTrigger");
 }
 
-
 void processing_screen()
-{ 
+{
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setCursor(40, 80);
   M5.Lcd.setTextSize(4);
@@ -136,7 +141,7 @@ void processing_screen()
 }
 
 void lnbits_screen()
-{ 
+{
   M5.Lcd.fillScreen(WHITE);
   M5.Lcd.setCursor(10, 90);
   M5.Lcd.setTextSize(3);
@@ -145,7 +150,7 @@ void lnbits_screen()
 }
 
 void portal_screen()
-{ 
+{
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setCursor(30, 80);
   M5.Lcd.setTextSize(3);
@@ -154,7 +159,7 @@ void portal_screen()
 }
 
 void complete_screen()
-{ 
+{
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setCursor(60, 80);
   M5.Lcd.setTextSize(4);
@@ -163,7 +168,7 @@ void complete_screen()
 }
 
 void error_no_invoice_key_screen()
-{ 
+{
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setCursor(70, 80);
   M5.Lcd.setTextSize(2);
@@ -172,7 +177,7 @@ void error_no_invoice_key_screen()
 }
 
 void error_no_connection_screen()
-{ 
+{
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setCursor(70, 80);
   M5.Lcd.setTextSize(2);
@@ -181,92 +186,102 @@ void error_no_connection_screen()
 }
 
 void qrdisplay_screen()
-{  
-  M5.Lcd.fillScreen(BLACK); 
-  M5.Lcd.qrcode(payReq,45,0,240,14);
+{
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.qrcode(payReq, 45, 0, 240, 14);
   delay(100);
 }
 
 /*               NODE CALLS                 */
 
-void getinvoice() {
-  const char* lnbitsserver = lnbits_server;
-  const char* lnbitsport = lnbits_port;
-  const char* invoicekey = invoice_key;
-  const char* lnbitsamount = lnbits_amount;
-  const char* lnbitsdescription = lnbits_description;
+void getinvoice()
+{
+  const char *lnbitsserver = lnbits_server;
+  const char *lnbitsport = lnbits_port;
+  const char *invoicekey = invoice_key;
+  const char *lnbitsamount = lnbits_amount;
+  const char *lnbitsdescription = lnbits_description;
   bool ssl = StartsWith(lnbitsserver, "https://");
-  WiFiClient* client;
+  WiFiClient *client;
   client = (ssl) ? new WiFiClientSecure() : new WiFiClient();
 
-  if (!client->connect(lnbitsserver, parseInt(lnbitsport))){
+  if (!client->connect(lnbitsserver, parseInt(lnbitsport)))
+  {
     down = true;
-    return;   
+    return;
   }
 
-  String topost = "{\"out\": false,\"amount\" : " + String(lnbitsamount) + ", \"memo\" :\""+ String(lnbitsdescription) + String(random(1,1000)) + "\"}";
+  String topost = "{\"out\": false,\"amount\" : " + String(lnbitsamount) + ", \"memo\" :\"" + String(lnbitsdescription) + String(random(1, 1000)) + "\"}";
   String url = "/api/v1/payments";
-  client->print(String("POST ") + url +" HTTP/1.1\r\n" +
+  client->print(String("POST ") + url + " HTTP/1.1\r\n" +
                 "Host: " + lnbitsserver + "\r\n" +
                 "User-Agent: ESP32\r\n" +
-                "X-Api-Key: "+ invoicekey +" \r\n" +
+                "X-Api-Key: " + invoicekey + " \r\n" +
                 "Content-Type: application/json\r\n" +
                 "Connection: close\r\n" +
                 "Content-Length: " + topost.length() + "\r\n" +
-                "\r\n" + 
+                "\r\n" +
                 topost + "\n");
-  while (client->connected()) {
+  while (client->connected())
+  {
     String line = client->readStringUntil('\n');
-    if (line == "\r") {
+    if (line == "\r")
+    {
       break;
     }
-    if (line == "\r") {
+    if (line == "\r")
+    {
       break;
     }
   }
-  
+
   String line = client->readString();
 
   StaticJsonDocument<1000> doc;
   DeserializationError error = deserializeJson(doc, line);
-  if (error) {
+  if (error)
+  {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.f_str());
     return;
   }
-  const char* payment_hash = doc["checking_id"];
-  const char* payment_request = doc["payment_request"];
+  const char *payment_hash = doc["checking_id"];
+  const char *payment_request = doc["payment_request"];
   payReq = payment_request;
   dataId = payment_hash;
 }
 
-
-void checkinvoice(){
-  const char* lnbitsserver = lnbits_server;
-  const char* lnbitsport = lnbits_port;
-  const char* invoicekey = invoice_key;
+void checkinvoice()
+{
+  const char *lnbitsserver = lnbits_server;
+  const char *lnbitsport = lnbits_port;
+  const char *invoicekey = invoice_key;
   bool ssl = StartsWith(lnbitsserver, "https://");
-  WiFiClient* client;
+  WiFiClient *client;
   client = (ssl) ? new WiFiClientSecure() : new WiFiClient();
 
-  if (!client->connect(lnbitsserver, parseInt(lnbitsport))){
+  if (!client->connect(lnbitsserver, parseInt(lnbitsport)))
+  {
     down = true;
-    return;   
+    return;
   }
 
   String url = "/api/v1/payments/";
-  client->print(String("GET ") + url + dataId +" HTTP/1.1\r\n" +
+  client->print(String("GET ") + url + dataId + " HTTP/1.1\r\n" +
                 "Host: " + lnbitsserver + "\r\n" +
                 "User-Agent: ESP32\r\n" +
-                "X-Api-Key:"+ invoicekey +"\r\n" +
+                "X-Api-Key:" + invoicekey + "\r\n" +
                 "Content-Type: application/json\r\n" +
                 "Connection: close\r\n\r\n");
-   while (client->connected()) {
-   String line = client->readStringUntil('\n');
-    if (line == "\r") {
+  while (client->connected())
+  {
+    String line = client->readStringUntil('\n');
+    if (line == "\r")
+    {
       break;
     }
-    if (line == "\r") {
+    if (line == "\r")
+    {
       break;
     }
   }
@@ -274,7 +289,8 @@ void checkinvoice(){
   Serial.println(line);
   StaticJsonDocument<200> doc;
   DeserializationError error = deserializeJson(doc, line);
-  if (error) {
+  if (error)
+  {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.f_str());
     return;
@@ -283,24 +299,28 @@ void checkinvoice(){
   paid = charPaid;
 }
 
-void portal(){
+void portal()
+{
 
   WiFiManager wm;
   Serial.println("mounting FS...");
-  while(!SPIFFS.begin(true)){
+  while (!SPIFFS.begin(true))
+  {
     Serial.println("failed to mount FS");
     delay(200);
-   }
+  }
 
   // Check if a reset was triggered, wipe data if it was
-  for (int i = 0; i <= 100; i++) {
-    if (M5.BtnA.wasPressed()){
-    portal_screen();
-    delay(1000);
-    File file = SPIFFS.open("/config.txt", FILE_WRITE);
-    file.print("placeholder");
-    wm.resetSettings();
-    i = 100;
+  for (int i = 0; i <= 100; i++)
+  {
+    if (M5.BtnA.wasPressed())
+    {
+      portal_screen();
+      delay(1000);
+      File file = SPIFFS.open("/config.txt", FILE_WRITE);
+      file.print("placeholder");
+      wm.resetSettings();
+      i = 100;
     }
     delay(50);
     M5.update();
@@ -308,12 +328,13 @@ void portal(){
 
   // Mount the file system and read config.txt
   File file = SPIFFS.open("/config.txt");
-  
+
   spiffing = file.readStringUntil('\n');
   spiffcontent = spiffing.c_str();
   DynamicJsonDocument json(1024);
   deserializeJson(json, spiffcontent);
-  if(String(spiffcontent) != "placeholder"){
+  if (String(spiffcontent) != "placeholder")
+  {
     strcpy(lnbits_server, json["lnbits_server"]);
     strcpy(lnbits_port, json["lnbits_port"]);
     strcpy(lnbits_description, json["lnbits_description"]);
@@ -325,7 +346,7 @@ void portal(){
 
   // ADD PARAMS TO WIFIMANAGER
   wm.setSaveConfigCallback(saveConfigCallback);
-  
+
   WiFiManagerParameter custom_lnbits_server("server", "LNbits server", lnbits_server, 40);
   WiFiManagerParameter custom_lnbits_port("port", "LNbits port", lnbits_port, 10);
   WiFiManagerParameter custom_lnbits_description("description", "Memo", lnbits_description, 200);
@@ -341,8 +362,9 @@ void portal(){
   wm.addParameter(&custom_high_pin);
   wm.addParameter(&custom_time_pin);
 
-  IF RESET WAS TRIGGERED, RUN PORTAL AND WRITE FILES
-  if (!wm.autoConnect("⚡lntrigger⚡", "password1")) {
+  // If a reset was triggered, run the portal and write files
+  if (!wm.autoConnect("⚡lntrigger⚡", "password1"))
+  {
     Serial.println("Failed to connect and hit timeout.");
     delay(3000);
     ESP.restart();
@@ -356,34 +378,37 @@ void portal(){
   strcpy(lnbits_amount, custom_lnbits_amount.getValue());
   strcpy(high_pin, custom_high_pin.getValue());
   strcpy(time_pin, custom_time_pin.getValue());
-  if (shouldSaveConfig) {
+  if (shouldSaveConfig)
+  {
     Serial.println("saving config");
     DynamicJsonDocument json(1024);
     json["lnbits_server"] = lnbits_server;
     json["lnbits_port"] = lnbits_port;
-    json["lnbits_description"]   = lnbits_description;
-    json["invoice_key"]   = invoice_key;
+    json["lnbits_description"] = lnbits_description;
+    json["invoice_key"] = invoice_key;
     json["lnbits_amount"] = lnbits_amount;
-    json["high_pin"]   = high_pin;
-    json["time_pin"]   = time_pin;
+    json["high_pin"] = high_pin;
+    json["time_pin"] = time_pin;
 
     File configFile = SPIFFS.open("/config.txt", "w");
-    if (!configFile) {
+    if (!configFile)
+    {
       Serial.println("Failed to open config file for writing.");
-      }
-      serializeJsonPretty(json, Serial);
-      serializeJson(json, configFile);
-      configFile.close();
-      shouldSaveConfig = false;
+    }
+    serializeJsonPretty(json, Serial);
+    serializeJson(json, configFile);
+    configFile.close();
+    shouldSaveConfig = false;
   }
 
-  Serial.println("local ip");
+  Serial.println("Local IP");
   Serial.println(WiFi.localIP());
   Serial.println(WiFi.gatewayIP());
   Serial.println(WiFi.subnetMask());
 }
 
-void saveConfigCallback () {
+void saveConfigCallback()
+{
   processing_screen();
   Serial.println("Should save config");
   shouldSaveConfig = true;
